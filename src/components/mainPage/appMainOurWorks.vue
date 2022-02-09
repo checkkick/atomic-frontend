ee8f8947d67054467ac6d2d342477736060fb952
checkkick work <tuskancik226@gmail.com>
2022-02-09 16:42:53 +0300

    add new swiper and finish 'our works'

--- src/components/mainPage/appMainOurWorks.vue
+++ src/components/mainPage/appMainOurWorks.vue
@@ -1,6 +1,43 @@
 <script setup>
-let model = 0
-let colors = ['primary', 'secondary', 'yellow darken-2', 'red', 'orange']
+import AppButtonComponent from '@/components/appButtonComponent.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/scrollbar'
+import 'swiper/css/free-mode'
+
+// import required modules
+import { Scrollbar, FreeMode } from 'swiper'
+
+let modules = [Scrollbar, FreeMode]
+
+let slides = [
+	{
+		text: 'Pringles',
+		href: 'red',
+	},
+	{
+		text: 'Arc’teryx',
+		href: 'red',
+	},
+	{
+		text: 'Kinder',
+		href: '',
+	},
+	{
+		text: 'Pringles',
+		href: 'red',
+	},
+	{
+		text: 'Arc’teryx',
+		href: 'red',
+	},
+	{
+		text: 'Kinder',
+		href: '',
+	},
+]
 </script>
 
 <template>
@@ -9,15 +46,22 @@ let colors = ['primary', 'secondary', 'yellow darken-2', 'red', 'orange']
 			<h1 class="works__title__text">Наши работы</h1>
 		</div>
 		<div class="works__examples">
-			<v-carousel v-model="model">
-				<v-carousel-item v-for="(color, i) in colors" :key="color">
-					<v-sheet :color="color" height="100%" tile>
-						<v-row class="fill-height" align="center" justify="center">
-							<div class="text-h2">Slide {{ i + 1 }}</div>
-						</v-row>
-					</v-sheet>
-				</v-carousel-item>
-			</v-carousel>
+			<swiper
+				:scrollbar="{
+					hide: false,
+				}"
+				:slides-per-view="3"
+				:space-between="0"
+				:modules="modules"
+				:free-mode="true"
+				class="swiper">
+				<swiper-slide v-for="(item, idx) in slides" :key="idx" class="swiper-slide">
+					<div class="swiper-slide__section">
+						<h2>{{ item.text }}</h2>
+						<AppButtonComponent text="Посмотреть" />
+					</div>
+				</swiper-slide>
+			</swiper>
 		</div>
 	</section>
 </template>
@@ -34,6 +78,47 @@ let colors = ['primary', 'secondary', 'yellow darken-2', 'red', 'orange']
 	font-size: $font-size-large + 10px;
 }
 .works__examples {
+	width: 100%;
+	display: inline-block;
 	border: $border-in-dark;
+}
+.swiper-slide {
+	transition: all 1s;
+	cursor: grab;
+	border-right: $border-in-dark;
+}
+.swiper-slide::before {
+	position: absolute;
+	content: '';
+	top: 0;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	background: url('@/assets/background-work.svg');
+	z-index: -1;
+	transition: opacity 0.5s linear;
+	opacity: 0;
+}
+.swiper-slide:hover::before {
+	opacity: 1;
+}
+.swiper-slide__section {
+	margin: $margin-default;
+	min-height: 40vh;
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	justify-content: space-between;
+}
+::v-deep .swiper-scrollbar {
+	left: 0;
+	bottom: 0;
+	width: 100%;
+	background: #3c3c3c;
+	border-radius: 0;
+}
+::v-deep .swiper-scrollbar-drag {
+	border-radius: 0;
+	background: white;
 }
 </style>
